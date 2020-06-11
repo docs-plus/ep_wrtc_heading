90707c3fc198460c3f20fc08b855e2473927f3f1
hmarzban <marzban98@gmail.com>
2020-06-11 12:13:15 +0430

    fix: disappear chat room when change header

--- static/js/shared.js
+++ static/js/shared.js
@@ -4,14 +4,20 @@ var tags = ["h1", "h2", "h3", "h4", "code"]
 var collectContentPre = function (hook, context) {
 	var tname = context.tname
 	var tagIndex = tags.indexOf(tname)
-	if (tagIndex >= 0) {
-		var headingTagId = randomString(16)
-		// when copy header form clipboard, trige headingTagId to set
-		context.cc.doAttrib(context.state, "headingTagId::" + headingTagId)
-		setTimeout(function () {
-			WRTC_Room.findTags()
-		}, 250)
+	var existTagId = /(?:^| )headingTagId_([A-Za-z0-9]*)/.exec(context.cls);
+	var jsx = /(?:^| )jsx-([A-Za-z0-9]*)/.exec(context.cls);
+
+	if(existTagId && existTagId[1]){
+		context.cc.doAttrib(context.state, "headingTagId::" + existTagId[1])
 	}
+
+	// when copy header form clipboard, trige headingTagId to set
+	if (tagIndex >= 0 && jsx && jsx[1]) {
+			context.cc.doAttrib(context.state, "headingTagId::" + randomString(16))
+	}
+	setTimeout(function () {
+		WRTC_Room.findTags()
+	}, 250)
 }
 
 exports.collectContentPre = collectContentPre
