106823f4a0ab08c74d89576a67c13f64c18437ab
hmarzban <marzban98@gmail.com>
2020-06-11 12:12:21 +0430

    refactor cycle

--- static/js/index.js
+++ static/js/index.js
@@ -9,84 +9,74 @@ var cssFiles = ["ep_wrtc_heading/static/css/wrtcRoom.css"];
 /*                              Plugin                                  */
 /************************************************************************/
 
-function ep_wrtcHeading(context) {
-	this.container = null;
-	this.padOuter = null;
-	this.padInner = null;
-	this.padId = clientVars.padId;
-	this.init();
-}
-
-ep_wrtcHeading.prototype.init = function () {
-	var _self = this;
-	this.findContainers();
-	this.insertContainers();
-
-	$("#options-wrtc-heading").on("change", function () {
-		$("#options-wrtc-heading").is(":checked") ? enableWrtcHeading() : disableWrtcHeading();
-	});
+var EPwrtcHeading = (function () {
+	var padId = clientVars.padId;
+	var padOuter = null;
+	var padInner = null;
+	var outerBody = null;
 
 	function enableWrtcHeading() {
-		_self.padOuter.find("#wbrtc_chatBox").addClass("active");
-		$("#rtcbox").addClass("active")
+		padOuter.find("#wbrtc_chatBox").addClass("active");
+		$("#rtcbox").addClass("active");
 	}
 
 	function disableWrtcHeading() {
-		_self.padOuter.find("#wbrtc_chatBox").removeClass("active");
-		$("#rtcbox").removeClass("active")
-		WRTC_Room.hangupAll()
+		padOuter.find("#wbrtc_chatBox").removeClass("active");
+		$("#rtcbox").removeClass("active");
+		WRTC_Room.hangupAll();
 	}
 
-	$("#options-wrtc-heading").trigger("change");
+	function init() {
 
-	WRTC_Room.initSocketJoin();
-};
+		// find containers
+		padOuter = $('iframe[name="ace_outer"]').contents();
+		padInner = padOuter.find('iframe[name="ace_inner"]');
+		outerBody = padOuter.find("#outerdocbody");
 
-ep_wrtcHeading.prototype.getUserFromId = function (userId) {
-	if (!this._pad || !this._pad.collabClient) return null;
-	var result = this._pad.collabClient.getConnectedUsers().filter(function (user) {
-		return user.userId === userId;
+		// insert wbrtc containers
+		var $target = outerBody;
+		if ($target.find("#wbrtc_chatBox").length > 0) return false;
+		$target.prepend('<div id="wbrtc_chatBox"></div>');
+
+		// module settings
+		$("#options-wrtc-heading").on("change", function () {
+			$("#options-wrtc-heading").is(":checked") ? enableWrtcHeading() : disableWrtcHeading();
+		});
+
+		$("#options-wrtc-heading").trigger("change");
+
+		WRTC_Room.initSocketJoin();
+	}
+
+	// Set all video_heading to be inline with their target REP
+	function setYofHeadingBox () {
+		var $padOuter = padOuter;
+		var $padInner = padInner;
+
+		$padOuter.find(".wbrtc_roomBox").each(function (index) {
+			var $boxId = $(this).attr("id");
+			var hClassId = "headingTagId_" + $boxId.split("_")[2];
+			var aceOuterPadding = parseInt($padInner.css("padding-top"));
+			var $headingEl = $padOuter.find("iframe").contents().find("#innerdocbody").find("." + hClassId);
+
+			// if the H tags does not find remove chatBox
+			// TODO: and kick out the user form the chatBox
+			if ($headingEl.length <= 0) {
+				$(this).remove();
+				return false;
+			}
+
+			var offsetTop = Math.floor($headingEl.offset().top + aceOuterPadding);
+
+			$(this).css({ top: offsetTop + "px" });
+		});
+	};
+
+	return Object.freeze({
+		init: init,
+		setYofHeadingBox: setYofHeadingBox
 	});
-	var user = result.length > 0 ? result[0] : null;
-	return user;
-};
-
-ep_wrtcHeading.prototype.findContainers = function () {
-	var padOuter = $('iframe[name="ace_outer"]').contents();
-	this.padOuter = padOuter;
-	this.padInner = padOuter.find('iframe[name="ace_inner"]');
-	this.outerBody = padOuter.find("#outerdocbody");
-};
-
-ep_wrtcHeading.prototype.insertContainers = function () {
-	var $target = this.outerBody;
-	if ($target.find("#wbrtc_chatBox").length > 0) return false;
-	$target.prepend('<div id="wbrtc_chatBox"></div>');
-};
-
-// Set all video_heading to be inline with their target REP
-ep_wrtcHeading.prototype.setYofHeadingBox = function () {
-	var $padOuter = this.padOuter;
-	var $padInner = this.padInner;
-
-	$padOuter.find(".wbrtc_roomBox").each(function (index) {
-		var $boxId = $(this).attr("id");
-		var hClassId = "headingTagId_" + $boxId.split("_")[2];
-		var aceOuterPadding = parseInt($padInner.css("padding-top"));
-		var $headingEl = $padOuter.find("iframe").contents().find("#innerdocbody").find("." + hClassId);
-
-		// if the H tags does not find remove chatBox
-		// TODO: and kick out the user form the chatBox
-		if ($headingEl.length <= 0) {
-			$(this).remove();
-			return false;
-		}
-
-		var offsetTop = Math.floor($headingEl.offset().top + aceOuterPadding);
-
-		$(this).css({ top: offsetTop + "px" });
-	});
-};
+})();
 
 /************************************************************************/
 /*                           Etherpad Hooks                             */
@@ -94,9 +84,7 @@ ep_wrtcHeading.prototype.setYofHeadingBox = function () {
 var hooks = {
 	postAceInit: function postAceInit(hook, context) {
 		if (!pad.plugins) pad.plugins = {};
-		var WRTCHeading = new ep_wrtcHeading(context);
-		this.WRTCHeading = WRTCHeading;
-		pad.plugins.ep_wrtc_heading = WRTCHeading;
+		pad.plugins.ep_wrtc_heading = EPwrtcHeading;
 
 		if (!$("#editorcontainerbox").hasClass("flex-layout")) {
 			$.gritter.add({
@@ -107,45 +95,45 @@ var hooks = {
 			});
 		}
 
+		EPwrtcHeading.init();
 		WRTC.postAceInit(hook, context);
-		WRTCHeading.init();
 		WRTC_Room.init(context);
 
-		$("#editorcontainer iframe").ready(function() {
+		$("#editorcontainer iframe").ready(function () {
 			setTimeout(function () {
 				WRTC_Room.findTags();
-			}, 150);
-		})
+			}, 250);
+		});
 
 		$(window).resize(_.debounce(function () {
-			WRTCHeading.setYofHeadingBox();
+			EPwrtcHeading.setYofHeadingBox();
 		}, 100));
-	
 	},
 	aceEditEvent: function aceEditEvent(hook, context) {
 		var eventType = context.callstack.editEvent.eventType;
 
-		// if in insert heading mode or when press key with new line
-		if ("setup,importText,setBaseText,setWraps".includes(eventType)) return;
+		// ignore these types
+		if ("handleClick,idleWorkTimer,setup,importText,setBaseText,setWraps".includes(eventType)) return;
 
-		// some times on init ep_wrtc_heading is not yet on the plugin list
+		// some times init ep_wrtc_heading is not yet in the plugin list
 		if (context.callstack.docTextChanged && pad.plugins.ep_wrtc_heading) pad.plugins.ep_wrtc_heading.setYofHeadingBox();
 
 		// apply changes to the other user
 		if (eventType === "applyChangesToBase" && context.callstack.selectionAffected) {
 			setTimeout(function () {
 				WRTC_Room.findTags();
-			}, 200);
+			}, 250);
 		}
 
 		// if user create a new heading, depend on ep_headings2
 		if (eventType === "insertheading") {
+			console.log(eventType,"===============")
 			// unfortunately "setAttributesRange" takes a little time to set attribute
 			// also ep_headings2 plugin has setTimeout about 250 ms to set and update H tag
 			// more info: https://github.com/ether/ep_headings2/blob/6827f1f0b64d99c3f3082bc0477d87187073a74f/static/js/index.js#L71
 			setTimeout(function () {
 				WRTC_Room.findTags();
-			}, 200);
+			}, 250);
 		}
 	},
 	aceAttribsToClasses: function aceAttribsToClasses(hook, context) {
