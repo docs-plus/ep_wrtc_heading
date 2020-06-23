9ade92b9cba1a6d3e2ae730ac7ce92035015397d
Hossein Marzban <marzban98@gmail.com>
2020-06-23 11:11:39 +0430

    fix: overflow mouseover, and responsive issues

--- static/js/webrtcRoom.js
+++ static/js/webrtcRoom.js
@@ -137,13 +137,15 @@ var WRTC_Room = (function () {
 
 			$headingRoom.find(".userCoutn").text(userCount);
 
+			var videoIcon = '<span class="videoIcon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="video" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-video fa-w-18 fa-2x"><path fill="currentColor" d="M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z" class=""></path></svg></span>';
+
+			// notify a user join the video-chat room
 			var msg = {
 				time: new Date(),
 				userId: user.userId,
-				text: "has joined the video-chat <b>" + headerText + "</b> <a class='txt_shareLink' href='" + self.createSahreLink(data.headingId) + "'>Join</a>",
+				text: videoIcon + headerText + " <a class='txt_shareLink' href='" + self.createSahreLink(data.headingId) + "'>Join</a>",
 				userName: user.name
 			};
-			// notify a user join the video-chat room
 			self.addTextChatMessage(msg);
 
 			if (data.headingId === currentUserRoom.headingId && data.userId !== clientVars.userId) {
@@ -264,15 +266,13 @@ var WRTC_Room = (function () {
 
 			self.$body_ace_outer().on("mouseover", ".wbrtc_roomBox", function () {
 				var $this = $(this);
-				self.$body_ace_outer().find("#wbrtc_chatBox").css({ overflow: "inherit" }).animate({}, 10, function () {
-					$this.addClass("active").find(".wbrtc_roomBoxFooter, .wbrtc_roomBoxBody, .wbrtc_roomBoxHeader b").show('fast');
-				});
+				self.$body_ace_outer().find("#wbrtc_chatBox").css({ overflow: "inherit" });
+				$this.addClass("active").find(".wbrtc_roomBoxFooter, .wbrtc_roomBoxBody, .wbrtc_roomBoxHeader b").css({display: "block"});
 			});
 
 			self.$body_ace_outer().on("mouseleave", ".wbrtc_roomBox", function () {
-				$(this).removeClass("active").find(".wbrtc_roomBoxFooter, .wbrtc_roomBoxBody, .wbrtc_roomBoxHeader b").css({ display: "none" }).animate({ display: "none" }, 10, function () {
-					self.$body_ace_outer().find("#wbrtc_chatBox").css({ overflow: "hidden" });
-				});
+				$(this).removeClass("active").find(".wbrtc_roomBoxFooter, .wbrtc_roomBoxBody, .wbrtc_roomBoxHeader b").css({ display: "none" });
+				self.$body_ace_outer().find("#wbrtc_chatBox").css({ overflow: "hidden" });
 			});
 
 			self.$body_ace_outer().on("click", ".wbrtc_roomBoxFooter > button.btn_door", function () {
