390478be0f2eecd4dd10de4a324875f499b55175
Hossein M <marzban98@gmail.com>
2020-11-18 17:18:40 +0330

    fix: socket notice open

--- static/js/index.js
+++ static/js/index.js
@@ -47,6 +47,7 @@ var EPwrtcHeading = (function EPwrtcHeading() {
 
 		// unfortunately when reconnection happen, etherpad break down totally
 		socket.on('connect', function() {
+			share.wrtcPubsub.emit('socket state', 'OPEND');
 			console.info('[wrtc]: socket connect', socket.id);
 		});
 		
