fb4600cbe329cc5ccd1a0f32297a03acea11e47c
Hossein <marzban98@gmail.com>
2021-12-09 10:06:10 +0330

    fix: limit/display inline icon from h1 to h4

--- static/js/index.js
+++ static/js/index.js
@@ -277,30 +277,33 @@ exports.aceInitialized = hooks.aceInitialized;
 //     }, 250);
 //   }
 // };
+const hTags = ["h1","h2","h3","h4"]
 // TODO: refactor
 exports.aceDomLineProcessLineAttributes = (name, context) => {
   const cls = context.cls;
-  // const videoHEaderType = /(?:^| )headingTagId_([A-Za-z0-9]*)/.exec(cls);
   const headingType = /(?:^| )headerId:([A-Za-z0-9]*)/.exec(cls);
   const result = [];
   // if (typeof Helper === 'undefined') return result;
 
   if (headingType) {
+    const headerType = /(?:^| )heading:([A-Za-z0-9]*)/.exec(cls);
     const headerId = headingType[1];
-      // if video or textChat modal is open! update modal title
-      if (Helper.wrtcStore.components.video.open || Helper.wrtcStore.components.text.open) {
-        const $header = Helper.findAceHeaderElement(headerId);
-        Helper.wrtcPubsub.emit('updateWrtcToolbarTitleModal', $header.text, headerId);
-      }
+    const htagNum = headerType[1]
 
-      const modifier = {
-        preHtml: '',
-        postHtml: `<chat-inline-icon data-headerid="${headerId}"></chat-inline-icon>`,
-        processedMarker: true,
-      };
+    // if video or textChat modal is open! update modal title
+    if (Helper.wrtcStore.components.video.open || Helper.wrtcStore.components.text.open) {
+      const $header = Helper.findAceHeaderElement(headerId);
+      Helper.wrtcPubsub.emit('updateWrtcToolbarTitleModal', $header.text, headerId);
+    }
+
+    const modifier = {
+      preHtml: '',
+      postHtml: `<chat-inline-icon data-headerid="${headerId}"></chat-inline-icon>`,
+      processedMarker: true,
+    };
 
     Helper.wrtcStore.rooms.set(headerId, {VIDEO: {list: []}, TEXT: {list: []}, USERS: {}, headerCount: 0});
-    result.push(modifier);
+    if(hTags.includes(htagNum)) result.push(modifier);
   }
 
   return result;
